562f6649f653eeb72370fa51a6ed2632dac97522
kentDevFrontEnd <qiang412@gmail.com>
2021-02-20 21:44:13 +0700

    change .env mode

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const inititalRoute = require("./routes/initialData.route");
 
 // mongoose connect
 mongoose.connect(
-  process.env.MONGO_LOCAL,
+  process.env.MONGO_URI,
   {
     useNewUrlParser: true,
     useCreateIndex: true,
